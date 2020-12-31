4942d5aa20016e00be7707cd887eddb2d62da602
mlahutin <v-mlahutin@vrbo.com>
2020-12-31 14:05:44 +0200

    init: initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import Board from './Board'
+import {layout} from './layout';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const scoreRef = useRef(null);
+    return (
+        <div className="App" >
+            <header className="header">
+                Packman game
+            </header>
+            <Board />
+
+            <h3>Score:<span ref={scoreRef} id="score" /></h3>
+        </div>
+    )
 }
 
 export default App;
